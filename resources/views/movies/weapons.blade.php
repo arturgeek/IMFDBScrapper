c54a30fa149996251d0945698fb4f15c45b92847
Andres Morales <andres.morales@impartner.com>
2022-03-04 17:55:05 -0500

    improve visuals of guns

--- resources/views/movies/weapons.blade.php
+++ resources/views/movies/weapons.blade.php
@@ -1,17 +1,22 @@
 @extends('layouts.master')
 
-@section('title', 'Page Title')
+@section('title', "Weapons of $movieCleanName" )
 
-@section('sidebar')
+@section('header')
     @parent
     {{ $movieCleanName }}
+@stop
+
+@section('subheader')
+    @parent
+    Here is the list of weapons fr this movie
 @stop
 
 @section('content')
     @foreach ( $categories as $category => $weapons )
     <div class="weapons-category">
         <h2>{{ $category }}</h2>
-        <hr class="col-12 mb-5 mt-5">
+        <hr class="col-12 mb-3 mt-3">
         @include('layouts.weaponsgrid', ['weapons' => $weapons, 'addToFavoriteButton' => true]);
     </div>
     @endforeach
