acd5f76dda2a4740e500a22d8e6e6624113d30e0
Andres Morales <andres.morales@impartner.com>
2022-03-03 22:56:06 -0500

    Separate layouts depending of the scrapper

---
+++ resources/views/layouts/mercadolibre-master.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title', 'Mercadolibre Scrapper')
+
+@section('header')
+    <h2>Scrap the content of an Mercadolibre Product</h2>
+@stop
+
+@section('subheader')
+    @section('sidebar')
+    <h1>
+        Your products @show
+    </h1>
+@stop
---
+++ resources/views/layouts/weapons-master.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title', 'IMFDB Scrapper')
+
+@section('header')
+    <h2>Search your favorite wapons from the Internet Movie Firearms Database</h2>
+    <p>You can start searching by your favorite movies, we already added some examples for you from own our list..</p>
+    <ul class="icon-list">
+        <li><a href="{{ url('movie-weapons?movie=Captain_America:_The_First_Avenger') }}">Captain America: The First Avenger</a></li>
+        <li><a href="{{ url('movie-weapons?movie=Captain_America:_The_Winter_Soldier') }}">Captain America: The Winter Soldier</a></li>
+    </ul>
+@stop
+
+@section('subheader')
+    @section('sidebar')
+    <h1>
+        The weapons of @show
+    </h1>
+@stop
